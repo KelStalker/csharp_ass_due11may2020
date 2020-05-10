46b7d2a5e6f7482e15f441fef7eb62e1c5f5045f
Kel <kelstalker@hotmail.com>
2020-05-10 12:26:45 -0600

    gonna stash this

--- Program.cs
+++ Program.cs
@@ -6,33 +6,28 @@ namespace csharp_ass_due11may2020
     {
         public static void Main(string[] args)
         {
-            
-            
+            string userFile;
+            string userString;
 
             CreateFile;
             GetString;
 
         }
 
-        public static void CreateFile(){
+        public static void CreateFile(string fileName){
         // get and store the file name specified by the user
-
-        string userFile;
             Console.WriteLine("Enter a filename:");
             userFile = Console.ReadLine();
             Console.WriteLine("Your file is: " + userFile);
         
-        return userFile;
+    return fileName;
         }
-
         // 
-         public static void GetString(){
-            string userString;
+         public static void GetString(fileName){
             Console.WriteLine("Now, enter a string and I will parse it with spaces");
             userString = Console.ReadLine();
-
-            return userString;
          }
         }
 
     }
+}
