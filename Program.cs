7bfa1f1144afae643a1c1765633d8010dc417101
Kel <kelstalker@hotmail.com>
2020-05-09 19:06:16 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace csharp_ass_due11may2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a filename:");
+            string userFile = Console.ReadLine();
+            Console.WriteLine("Your file is: " + userFile);
+
+        }
+    }
+}
